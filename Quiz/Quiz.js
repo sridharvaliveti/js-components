b67a9699ea797a66c106d23019b3db06b289e990
rsadwick <rsadwick@gmail.com>
2013-05-07 15:34:53 -0400

    Quiz iterations

--- Quiz/Quiz.js
+++ Quiz/Quiz.js
@@ -8,67 +8,47 @@ var results = $("#results");
 var sharing = $("#fb-root");
 var share_btn = $('.like_btn');
 
-$(document).ready(function($) {
 
-    showQuestion = function(qid)
-    {
-        quiz.fadeIn();
-        questions.eq(qid).fadeIn();
-        questionCounter.text(currentQuestion + 1 +  " / " + questions.length);
+showQuestion = function(qid)
+{
+    quiz.fadeIn();
+    questions.eq(qid).fadeIn();
+    questionCounter.text(currentQuestion + 1 +  " / " + questions.length);
 
-        var answers = questions.eq(qid).find("li");
-        answers.css('cursor', 'pointer');
-        //events
-        answers.on({
-            'mouseenter' : function(e){
-                $(this).find(".answer_icon").css("backgroundPosition", "-79px 0px");
-            },
-            'mouseleave' : function(e){
-                $(this).find(".answer_icon").css("backgroundPosition", "1px 0px");
-            },
-            'click' : function(e){
-                userWeights.push($(this).data("weight"));
-                hideQuestion(currentQuestion);
-                currentQuestion++;
-                //end quiz or keep going:
-                (currentQuestion == questions.length) ? grade() : showQuestion(currentQuestion);
-            }
-        });
-    }
-
-    hideQuestion = function(qid)
-    {
-        questions.eq(qid).hide();
-    }
-
-    grade = function()
-    {
-        var totalWeight = 0;
-
-        for(var currentAnswer = 0; currentAnswer < userWeights.length; currentAnswer++)
-        {
-            totalWeight += userWeights[currentAnswer];
+    var answers = questions.eq(qid).find("li");
+    answers.css('cursor', 'pointer');
+    //events
+    answers.on({
+        'mouseenter' : function(e){
+            $(this).find(".answer_icon").css("backgroundPosition", "-79px 0px");
+        },
+        'mouseleave' : function(e){
+            $(this).find(".answer_icon").css("backgroundPosition", "1px 0px");
+        },
+        'click' : function(e){
+            userWeights.push($(this).data("weight"));
+            hideQuestion(currentQuestion);
+            currentQuestion++;
+            //end quiz or keep going:
+            (currentQuestion == questions.length) ? grade() : showQuestion(currentQuestion);
         }
+    });
+}
 
-        //Get the verses:
-        $.ajax({
-            url: '/quiz/ajax/' + quiz.data("category") + '/' + totalWeight,
-            success: function(data)
-            {
-                for(v = 0; v < data.length; v++)
-                {
-                    var bible_verses = $('<p class="bible_verses"></p>').html(data[v].verse)
-                    var bible_book = $('<p class="bible_book"></p>').html(data[v].book);
+hideQuestion = function(qid)
+{
+    questions.eq(qid).hide();
+}
 
-                    sharing.after(bible_verses);
-                    bible_verses.after(bible_book);
-                    bible_book.fadeIn();
-                }
-                //fade in results:
-                results.fadeIn();
-            }
-        });
+grade = function()
+{
+    var totalWeight = 0;
+    for(var currentAnswer = 0; currentAnswer < userWeights.length; currentAnswer++)
+    {
+        totalWeight += userWeights[currentAnswer];
     }
-    //Let There Be Quiz!
-    showQuestion(0);
-});
+
+    var results = quiz.find('#results');
+    results.show().html('total weight: ' + totalWeight);
+}
+
